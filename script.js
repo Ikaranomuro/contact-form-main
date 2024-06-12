6096adfa39054bf01cce157afc4208da4465091c
Iago Oliveira Fonseca <iagooliveirafonseca83@gmail.com>
2024-06-12 14:32:00 -0300

    Começando a adicionar os elemento visuais de erro ao não preencher o formulario

---
+++ script.js
@@ -0,0 +1,60 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function loaded(){
+ 
+//     const a = document.getElementById('paragraphRadio1')
+//     const but = document.getElementsByTagName('button')[0]
+//     a.style.backgroundColor = "Red"
+//     but.addEventListener('click', changingElements)
+
+//     function changingElements(){
+//     const w = document.getElementsByClassName('wrapper')[0]
+//     w.style.backgroundColor = "Red"
+//     }
+
+
+//     a.addEventListener('click', ativandoElemnt);
+//     function ativandoElemnt(){
+//         // const backgroundLabel = document.getElementsByClassName('take')[0]
+//         // backgroundLabel.style.backgroundColor = "Red"
+
+        
+//     }
+    
+// }
+
+   
